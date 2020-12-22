c5610d2f50bb5f46b985478880687bba721b78b9
Seun Odeyemi <blacng@gmail.com>
2020-12-22 18:37:45 -0500

    Changes to Secret Key

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yv0a(4&*(uun07$u!($%0)kyieggj7b*o@xj_+%@d_c(*)0!-z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
